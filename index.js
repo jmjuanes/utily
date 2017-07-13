b83292205617005253bac4b1fc8b9043c51c5875
jmjuanes <josemijuanes@gmail.com>
2017-07-13 16:29:51 +0200

    index.js: import json methods

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports.file = require('./lib/file.js');
 //Import type checker functions
 module.exports.is = require('./lib/is.js');
 
+//Import json functions
+module.exports.json = require('./lib/json.js');
+
 //Import object functions
 module.exports.object = require('./lib/object.js');
 
