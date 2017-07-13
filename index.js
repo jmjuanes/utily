bbf18fdd7047645bee7b240c1f7f0d89de084c8b
jmjuanes <josemijuanes@gmail.com>
2017-07-13 11:30:04 +0200

    index.js: import is and string from libs

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 //Import file functions
 module.exports.file = require('./lib/file.js');
 
+//Import type checker functions
+module.exports.is = require('./lib/is.js');
+
 //Import object functions
 module.exports.object = require('./lib/object.js');
+
+//Import string functions
+module.exports.string = require('./lib/string.js');
